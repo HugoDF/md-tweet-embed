750214d08764746d6e4f5c1fdbec4ac4ef19bf8c
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2022-04-26 16:01:30 +0100

    refactor to ESM, support Node 18 only (drop node-fetch)

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -1,20 +1,20 @@
-const {test} = require('uvu');
-const assert = require('uvu/assert');
-const {execSync} = require('child_process');
-const tweets = require('./fixtures');
+import {execSync} from 'node:child_process';
+import {test} from 'uvu';
+import {snapshot} from 'uvu/assert';
+import tweets from './fixtures.js';
 
 const runCli = (arg) => execSync(`./cli.js ${arg}`).toString().trimEnd();
 
 test('CLI - 1 tweet', () => {
-  assert.snapshot(runCli(tweets[0].url), tweets[0].markdown);
+  snapshot(runCli(tweets[0].url), tweets[0].markdown);
 });
 
 test('CLI - many tweets', () => {
-  assert.snapshot(
+  snapshot(
     runCli(`${tweets[0].url} ${tweets[1].url}`),
     `${tweets[0].markdown}
 -----
-${tweets[1].markdown}`
+${tweets[1].markdown}`,
   );
 });
 
